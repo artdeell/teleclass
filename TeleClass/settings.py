4ef7e690662f269d13a2449c9ed759f390529fb5
Jost221 <mr.z.75@mail.ru>
2022-12-20 18:34:19 +0300

    наидал базу

--- TeleClass/settings.py
+++ TeleClass/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'TeleClass.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['class\\templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
