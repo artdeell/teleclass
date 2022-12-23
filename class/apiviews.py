1bddb8ecab6ab2f3781ba7442e4f854f27199866
Jost221 <mr.z.75@mail.ru>
2022-12-23 04:21:04 +0300

    Добавлены: поиск курсов, отображение списка курсов, взятие школьником курса

--- class/apiviews.py
+++ class/apiviews.py
@@ -2,8 +2,7 @@ from django.http import HttpResponseRedirect
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-
-import json
+from django.core import serializers
 
 
 from .globalFunction import *
@@ -28,12 +27,37 @@ class Autorizations(APIView):
         parent = search_obj(Parent, **request.data)
         child = search_obj(Child, **request.data)
         if parent != None:
-            return Response({'number': parent.id, 'type':'parent'}, status=status.HTTP_200_OK)
+            return Response({'number': parent[0].id, 'type':'parent'}, status=status.HTTP_200_OK)
         elif child != None:
             return Response({'number': child[0].id, 'type':'child'}, status=status.HTTP_200_OK)
         return Response({'error': 'Не найдено пользователь с таким логином или паролем'}, status=status.HTTP_400_BAD_REQUEST)
 
 
 class Сourses(APIView):
-    def get(self, request):
-        return Response(search_all_obj(Course))
+    def get(self, request, id):
+        # courses = search_all_obj(Course)
+        # respons = []
+        # for cours in courses:
+        #     respons.append(
+        #         {
+        #             'name': name
+        #         }
+        #     )
+        return Response(serializers.serialize('json', search_all_obj(Course)))
+
+    def post(self, request, id):
+        return Response(serializers.serialize('json', search_obj(Course, **request.data)))
+
+class TakeCourse(APIView):
+    def post(self, request, id):
+        cours = search_obj(Course, name=request.data['name'])[0]
+        if request.data['user_type']=='chlid':
+            child = search_obj(Child, id=id)
+            if len(search_obj(ActualCourse, child=child))>0:
+                return Response({'error': 'Данный курс был выбран для прохождения ранее'}, status=status.HTTP_400_BAD_REQUEST)
+            act_cours = create_obj(ActualCourse, course=cours, mark=0, child=child)
+            tasks = search_obj(Task, course=cours)
+            for task in tasks:
+                create_obj(ActualTask, actual_course=act_cours, task=task, mark=task.mark, time=0, count=0, status=False)
+            return Response({id: act_cours.id}, status=status.HTTP_200_OK)
+                
--- class/urls.py
+++ class/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('auth/', views.auth),
     path('api/authorizations', apiviews.Autorizations.as_view()),
     path('api/registrations', apiviews.Registraton.as_view()),
-    path('courses/', apiviews.Сourses.as_view()),
+    path('api/courses/<int:id>/list', apiviews.Сourses.as_view()),
+    path('api/courses/<int:id>/take', apiviews.Сourses.as_view()),
     path('home/<int:id>/', views.auth)#Поменять
 ]
