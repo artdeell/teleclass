bcaf5f0133ef0182557e799c2e22602709dd0483
Jost221 <mr.z.75@mail.ru>
2022-12-24 07:50:09 +0300

    eto cacoyto cringe

--- static/js/catalog.js
+++ static/js/catalog.js
@@ -1,28 +1,28 @@
-window.onload = function(){
+window.onload = function () {
     const request = new XMLHttpRequest();
     const url = `${window.location.origin}/api/courses/${window.location.pathname.replace("/catalog/", "")}list`;
-    
+
     console.log(url)
     request.open('GET', url, true);
     request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
     request.send()
-    request.onload = ()=>{
+    request.onload = () => {
         response = JSON.parse(request.responseText)//словарь с турнирами
         console.log(response)
 
         catalog = document.querySelector(".catalog-row")
         courses_rendered = 0
-        if (1<=response['len']-courses_rendered) {
+        if (1 <= response['len'] - courses_rendered) {
             for (let course_number = response['len']; course_number > courses_rendered; course_number--) {
                 catalog.innerHTML += `
-                <div class="course" id = ${response[course_number-1]['id']}>
+                <div class="course" id = ${response[course_number - 1]['id']}>
                 <div class="course-row-top">
                     <div class="course-col">
                         <div class="course-title">
-                            ${response[course_number-1]['name']}
+                            ${response[course_number - 1]['name']}
                         </div>
                         <div class="course-description">
-                            ${response[course_number-1]['description']}
+                            ${response[course_number - 1]['description']}
                         </div>
                     </div>
                     <div class="course-icon">
@@ -31,9 +31,9 @@ window.onload = function(){
                 </div>
                 <div class="course-row-bot">
                     <div class="course-max-point">
-                        ${response[course_number-1]['points']}
+                        ${response[course_number - 1]['points']}
                     </div>
-                    <button class="course-open" id="${response[course_number-1]['id']}">
+                    <button class="course-open" id="${response[course_number - 1]['id']}">
                         Открыть курс
                     </button>
                 </div>
@@ -41,5 +41,28 @@ window.onload = function(){
                 `
             }
         }
-    } 
+        // pizdos()
+    }
+}
+// function pizdos() {
+//     document.querySelectorAll("button.course-open").forEach(button => {
+//         button.addEventListener('click', OpenCourse)
+//         console.log(button)
+//     })
+// }
+
+
+function OpenCourse(button) {
+    const request = new XMLHttpRequest();
+    const url = `${window.location.origin}/api/authorizations`;
+    request.open('POST', url, true);
+    request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    data = {
+        'id': Number(button.id)
+    }
+    console.log(data)
+    request.send(JSON.stringify(data))
+    request.onload = () => {
+        console.log(request.response)
+    }
 }
