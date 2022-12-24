74e79002662b2793e31563ce92898ca6264868ee
s1uad_dw <s1uad.dw@gmail.com>
2022-12-24 06:05:22 +0300

    auth as start-page

--- class/urls.py
+++ class/urls.py
@@ -5,7 +5,7 @@ from . import views
 from . import apiviews
 
 urlpatterns = [
-    path('auth/', views.auth),
+    path('', views.auth),
     path('catalog/<int:id>/', views.catalog),
     path('api/authorizations', apiviews.Autorizations.as_view()),
     path('api/registrations', apiviews.Registraton.as_view()),
