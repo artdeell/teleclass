7cb6dce7f2a055bed98c9e4a850fd58205ebf97e
Jost221 <mr.z.75@mail.ru>
2022-12-24 07:19:28 +0300

    ono poka rabotaet

--- static/js/catalog.js
+++ static/js/catalog.js
@@ -1,6 +1,7 @@
 window.onload = function(){
     const request = new XMLHttpRequest();
     const url = `${window.location.origin}/api/courses/${window.location.pathname.replace("/catalog/", "")}list`;
+    
     console.log(url)
     request.open('GET', url, true);
     request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
@@ -8,31 +9,37 @@ window.onload = function(){
     request.onload = ()=>{
         response = JSON.parse(request.responseText)//словарь с турнирами
         console.log(response)
-    }
-    document.querySelectorAll("button.course-open").forEach(button =>{
-        button.addEventListener('click', function(){
-            Open(button.id)
-        })
-    }) 
-}
 
-
-
-function Open(id){
-    const request = new XMLHttpRequest();
-    const url = `${window.location.origin}/api/courses/${window.location.pathname.replace("/catalog/", "")}take`;
-    console.log(url)
-    request.open('POST', url, true);
-    request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    data = {
-        'user_tipe': 'child',
-        'id': Number(id)
-    }
-    console.log(data)
-    request.send(JSON.stringify(data))
-    request.onload = ()=>{
-        response = JSON.parse(request.responseText)
-        console.log(response)
-
-    }
+        catalog = document.querySelector(".catalog-row")
+        courses_rendered = 0
+        if (1<=response['len']-courses_rendered) {
+            for (let course_number = response['len']; course_number > courses_rendered; course_number--) {
+                catalog.innerHTML += `
+                <div class="course" id = ${response[course_number-1]['id']}>
+                <div class="course-row-top">
+                    <div class="course-col">
+                        <div class="course-title">
+                            ${response[course_number-1]['name']}
+                        </div>
+                        <div class="course-description">
+                            ${response[course_number-1]['description']}
+                        </div>
+                    </div>
+                    <div class="course-icon">
+                        <img src="https://i.ibb.co/4TjWN2t/default-course-icon.png" class="course-icon-img">
+                    </div>
+                </div>
+                <div class="course-row-bot">
+                    <div class="course-max-point">
+                        ${response[course_number-1]['points']}
+                    </div>
+                    <button class="course-open" id="${response[course_number-1]['id']}">
+                        Открыть курс
+                    </button>
+                </div>
+            </div>
+                `
+            }
+        }
+    } 
 }
