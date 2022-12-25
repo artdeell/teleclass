f12b0ac55722b15eecff70106b4a08da5c2832a9
s1uad_dw <s1uad.dw@gmail.com>
2022-12-25 14:08:10 +0300

    microfix

--- class/urls.py
+++ class/urls.py
@@ -6,14 +6,17 @@ from . import apiviews
 
 urlpatterns = [
     path('', views.auth),
+    path('catalog/', views.catalog),
+    
+    path('catalog/course/<course_id>/', views.course),
+    path('student/<student_id>', views.personal_office_student),
+    path('parent/<parent_id>', views.personal_office_parent),
+    path('teacher/<teacher_id>', views.personal_office_teacher),
+
     path('api/authorizations', apiviews.Auth.as_view()),
     path('api/registrations', apiviews.Reg.as_view()),
     # path('api/courses/<int:id>/list', apiviews.Сourses.as_view()),
     # path('api/courses/<int:id>/take', apiviews.TakeCourse.as_view()),
     # path('home/<int:id>/', views.auth)#Поменять
-    path('catalog/course/<course_id>/', views.course),
-    path('student/<student_id>', views.personal_office_student),
-    path('parent/<parent_id>', views.personal_office_parent),
-    path('teacher/<teacher_id>', views.personal_office_teacher),
 ]
 
